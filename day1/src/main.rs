2a9ba823929923980adb85df87b79466be66678b
Daniel Steman <daniel.steman@bridgefund.nl>
2023-12-11 17:37:04 +0100

    working version with example input

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,17 +1,41 @@
 fn main() {
     let cases = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+    let mut total_sum = 0;
     for case in cases {
-        get_first_and_last_digit(String::from("case"))
+        if let Some(digits) = get_first_and_last_digit(String::from(case)) {
+            println!("first digit: {}", digits.first_digit);
+            println!("last digit: {}", digits.last_digit);
+            let result = digits.sum();
+            match result {
+                Ok(sum) => total_sum = total_sum + sum,
+                Err(_) => println!("couldn't parse sum"),
+            }
+        } else {
+            println!("no first and/or last digit found")
+        }
+    }
+    println!("total sum: {}", total_sum)
+}
+
+struct FirstLastDigits {
+    first_digit: i32,
+    last_digit: i32,
+}
+
+impl FirstLastDigits {
+    fn sum(&self) -> Result<i32, std::num::ParseIntError> {
+        let concatenated = format!("{}{}", &self.first_digit, &self.last_digit);
+        let sum = concatenated.parse::<i32>()?;
+        Ok(sum)
     }
 }
 
-fn get_first_and_last_digit(s: String) {
-    let chars = s.chars();
-    for c in chars {
-        if c.is_ascii_digit() {
-            println!("{}", c)
-        } else {
-            println!("Not a number")
-        }
-    }
+fn get_first_and_last_digit(s: String) -> Option<FirstLastDigits> {
+    let first_digit = s.chars().clone().find(|c| c.is_ascii_digit())?;
+    let last_digit = s.chars().clone().rev().find(|c| c.is_ascii_digit())?;
+
+    Some(FirstLastDigits {
+        first_digit: first_digit as i32 - '0' as i32,
+        last_digit: last_digit as i32 - '0' as i32,
+    })
 }
