2cae53513b2c8e111d4c4d58737f5ae6a7da3eb2
Daniel Steman <daniel.steman@bridgefund.nl>
2023-12-14 12:20:42 +0100

    get sets

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,23 +4,30 @@ fn main() {
     // let cases = read_lines("src/input.txt");
     let s = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
 
-    // let id = get_id(s);
-    // println!("id: {}", id)
+    let id = get_id(s);
+    println!("id: {}", id);
 
+    let sets = get_sets(s);
+    println!("sets: {:?}", sets);
+}
+
+fn get_sets(s: &str) -> Vec<HashMap<&str, &str>> {
     let parts: Vec<&str> = s.split(": ").collect();
+    let cubes = parts[1].split("; ");
+    let mut sets: Vec<HashMap<&str, &str>> = vec![];
 
-    let sets = parts[1].split("; ");
-
-    let mut map: HashMap<&str, &str> = HashMap::new();
-
-    for set in sets {
-        for cube in set.split(", ") {
+    for part in cubes {
+        let mut map: HashMap<&str, &str> = HashMap::new();
+        for cube in part.split(", ") {
             let cube_data: Vec<&str> = cube.split(" ").collect();
             let q = cube_data[0];
             let color = cube_data[1];
-            println!("{:?}", cube)
+            map.insert(color, q);
         }
+        sets.push(map);
     }
+
+    sets
 }
 
 fn get_id(s: &str) -> &str {
@@ -41,7 +48,7 @@ fn read_lines(filename: &str) -> Vec<String> {
 
 struct Game<'a> {
     id: i32,
-    sets: Vec<HashMap<i32, &'a str>>,
+    sets: Vec<HashMap<&'a str, &'a str>>,
 }
 
 // impl Game {
