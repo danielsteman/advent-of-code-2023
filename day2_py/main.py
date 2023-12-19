b3aa373bedae8529aaa72cbeb817c2883d416b7a
Daniel Steman <daniel.steman@bridgefund.nl>
2023-12-19 18:59:52 +0100

    nailed it

--- day2_py/main.py
+++ day2_py/main.py
@@ -1,64 +1,41 @@
-s = "Game 1: 3 blue, 4 red; 19 red, 2 green, 6 blue; 2 green"
+from collections import namedtuple
+from typing import List
 
-game = s.split(":")[0].split(" ")[-1]
-
-sets = []
-for str_set in s.split(": ")[1].split("; "):
-    set = {}
-    for cubes in str_set.split(", "):
-        unpacked_cubes = cubes.split(" ")
-        set[unpacked_cubes[1]] = int(unpacked_cubes[0])
-    sets.append(set)
-
-print(sets)
-
-rules = {
-    "red": 12,
-    "green": 13,
-    "blue": 14,
-}
-
-counter = {
-    "red": 0,
-    "green": 0,
-    "blue": 0,
-}
-
-for set in sets:
-    print(set)
-    for color, value in set.items():
-        counter[color] += value
-        if counter[color] > rules[color]:
-            print("impossible")
-            break
-
-print(counter)
+Cube = namedtuple("Cube", ["quantity", "color"])
 
 
 class Game:
     def __init__(self, s):
+        s = s.strip("\n")
         self.id = self.get_id(s)
-        self.sets = self.get_sets(s)
         self.rules = {
             "red": 12,
             "green": 13,
             "blue": 14,
         }
+        self.points = self.parse_and_count(s)
 
     @staticmethod
-    def get_id(self, s):
+    def get_id(s: str) -> List[str]:
         return s.split(":")[0].split(" ")[-1]
 
-    @staticmethod
-    def get_sets(self, s):
-        sets = []
+    def parse_and_count(self, s: str) -> int:
+        # count = {key: 0 for key in self.rules}
         for str_set in s.split(": ")[1].split("; "):
-            set = {}
             for cubes in str_set.split(", "):
-                unpacked_cubes = cubes.split(" ")
-                set[unpacked_cubes[1]] = int(unpacked_cubes[0])
-            sets.append(set)
-        return sets
+                unpacked_cubes = Cube(*cubes.split(" "))
+                # count[unpacked_cubes.color] += int(unpacked_cubes.quantity)
+                if int(unpacked_cubes.quantity) > self.rules[unpacked_cubes.color]:
+                    return 0
 
-    def is_possible(self):
-        return
+        return int(self.id)
+
+
+total_points = 0
+
+with open("input.txt", "r") as f:
+    for line in f.readlines():
+        game = Game(line)
+        total_points += game.points
+
+print(total_points)
