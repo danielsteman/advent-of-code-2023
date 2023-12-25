eb9c41388344a4da1a49a2ad1db837449c1290de
Daniel Steman <daniel-steman@live.nl>
2023-12-25 22:56:58 +0100

    quick and dirty

--- day4_py/main.py
+++ day4_py/main.py
@@ -1,11 +1,33 @@
-with open("input.txt") as file:
-    data = file.read()
-    numbers = data.split("|")
+def get_points(s: str) -> int:
+    temp = s.split(":")[1].strip().split("|")
 
-example = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-temp = example.split(":")
-card_id = temp[0]
-my_numbers = temp[1].strip().split("|")[0].strip().split(" ")
-winning_numbers = temp[1].strip().split("|")[1].strip().split(" ")
-print(my_numbers)
-print(winning_numbers)
+    my_numbers = temp[0].strip().split(" ")
+    my_numbers = [i for i in my_numbers if i]
+
+    winning_numbers = temp[1].strip().split(" ")
+    winning_numbers = [i for i in winning_numbers if i]
+
+    my_winning_numbers = set(my_numbers).intersection(set(winning_numbers))
+
+    count_winning_numbers = len(my_winning_numbers)
+    if count_winning_numbers == 0:
+        return 0
+
+    points = 2 ** (count_winning_numbers - 1)
+
+    return points
+
+
+total_points = 0
+
+with open("input.txt", "r") as file:
+    data = file.readlines()
+    for i in range(len(data)):
+        try:
+            points = get_points(data[i])
+            total_points += points
+        except Exception as e:
+            print(f"error row: {data[i]}. With error: {e}")
+
+
+print(total_points)
